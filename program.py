4f1444136a8bab8181a6cf0915cdad2eae3d9d64
IT-11 <it6kvantorium@gmail.com>
2023-11-17 18:11:35 +0700

    правки автора

--- program.py
+++ program.py
@@ -1,7 +1,13 @@
+# Yandexlyceum
+def my_superfunction():
+    print('What an awesome print!')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
